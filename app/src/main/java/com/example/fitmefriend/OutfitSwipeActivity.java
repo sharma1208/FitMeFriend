2ac7a1da3dc938bbea57a73830a6098d878fe78b
sharma1208 <90787231+sharma1208@users.noreply.github.com>
2022-10-31 12:52:49 -0500

    set up for all the screens

---
+++ app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitmefriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OutfitSwipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outfit_swipe);
+    }
+}
---
+++ app/src/main/java/com/example/fitmefriend/WardrobeMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitmefriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WardrobeMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wardrobe_menu);
+    }
+}

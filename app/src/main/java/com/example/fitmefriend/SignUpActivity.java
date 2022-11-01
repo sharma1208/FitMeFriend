4ed06e66d4617aba59495b3b71c378e6050602f9
vani123r <90778412+vani123r@users.noreply.github.com>
2022-11-01 12:51:04 -0500

    button from sign up to options

--- app/src/main/java/com/example/fitmefriend/SignUpActivity.java
+++ app/src/main/java/com/example/fitmefriend/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.example.fitmefriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
+
+    public void signUpButtonClicked(View view) {
+        Intent intent = new Intent(SignUpActivity.this, OptionsActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
 }

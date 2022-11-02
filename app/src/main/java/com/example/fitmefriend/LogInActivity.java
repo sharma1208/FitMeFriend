9385b21dce5870f7121f44aacd85c15ab17c44b0
vani123r <90778412+vani123r@users.noreply.github.com>
2022-11-02 12:13:46 -0500

    worked on on click

--- app/src/main/java/com/example/fitmefriend/LogInActivity.java
+++ app/src/main/java/com/example/fitmefriend/LogInActivity.java
@@ -2,7 +2,9 @@ package com.example.fitmefriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+    public void logInButtonClicked(View view) {
+        Intent intent = new Intent(LogInActivity.this, OptionsActivity.class);
+        startActivity(intent);
+
+
     }
 }

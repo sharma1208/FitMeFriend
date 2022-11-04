53d352c057fc6e82cb0075bd8c451fe953ea3532
vani123r <90778412+vani123r@users.noreply.github.com>
2022-11-04 12:36:25 -0500

    added a button

--- app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
+++ app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
@@ -2,7 +2,9 @@ package com.example.fitmefriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OutfitSwipeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class OutfitSwipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_swipe);
+    }
+    public void saveWardrobe(View view) {
+        Intent intent = new Intent(OutfitSwipeActivity.this, SavedOutfitsActivity.class);
+        startActivity(intent);
+
     }
 }

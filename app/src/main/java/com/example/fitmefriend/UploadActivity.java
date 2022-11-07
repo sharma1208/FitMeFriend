454e6d3f2dc4e36947ddaa67d9d298e32c5c6962
vani123r <90778412+vani123r@users.noreply.github.com>
2022-11-07 12:10:26 -0600

    Spinner

--- app/src/main/java/com/example/fitmefriend/UploadActivity.java
+++ app/src/main/java/com/example/fitmefriend/UploadActivity.java
@@ -3,12 +3,40 @@ package com.example.fitmefriend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class UploadActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    Spinner spinner;
+    String spinnerSelectedText = "none";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+        spinner = findViewById(R.id.mySpinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_list,
+                getResources().getStringArray(R.array.memoryRatings));
+
+        // this attaches my custom row design (how I want each row to look)
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        spinnerSelectedText = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), spinnerSelectedText, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 }

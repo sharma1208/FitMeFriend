9f9f39f0e384b94afa0f8554c963b53e8ca65dd8
vani123r <90778412+vani123r@users.noreply.github.com>
2022-11-10 12:52:20 -0600

    adapter stuff

--- app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
+++ app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
@@ -1,6 +1,8 @@
 package com.example.fitmefriend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,14 +10,22 @@ import android.view.View;
 
 public class OutfitSwipeActivity extends AppCompatActivity {
 
+    private RecyclerView shirtRecyclerView, pantRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_swipe);
+        shirtRecyclerView = findViewById(R.id.shirtRecyclerView);
+        shirtRecyclerView.setHasFixedSize(true);
+        pantRecyclerView = findViewById(R.id.pantRecyclerView);
+        pantRecyclerView.setHasFixedSize(true);
     }
     public void saveWardrobe(View view) {
         Intent intent = new Intent(OutfitSwipeActivity.this, SavedOutfitsActivity.class);
         startActivity(intent);
 
     }
+
+
+
 }

c7bddce2fba0700b32cfb3cd42a20e6ce37b3701
vani123r <90778412+vani123r@users.noreply.github.com>
2022-11-14 12:53:34 -0600

    got images

--- app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
+++ app/src/main/java/com/example/fitmefriend/OutfitSwipeActivity.java
@@ -3,6 +3,7 @@ package com.example.fitmefriend;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,6 +24,7 @@ public class OutfitSwipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_outfit_swipe);
         shirtRecyclerView = findViewById(R.id.shirtRecyclerView);
         shirtRecyclerView.setHasFixedSize(true);
+        shirtRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
         List<Shirts> shirtsList = new ArrayList<>();
 
         shirtsList.add(new Shirts(R.drawable.sampleimage));
@@ -31,6 +33,7 @@ public class OutfitSwipeActivity extends AppCompatActivity {
 
         pantRecyclerView = findViewById(R.id.pantRecyclerView);
         pantRecyclerView.setHasFixedSize(true);
+        pantRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
         List<Pants> pantsList = new ArrayList<>();
 
         pantsList.add(new Pants(R.drawable.sampleimage));
